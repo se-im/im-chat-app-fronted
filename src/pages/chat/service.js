086166521488d422c42b105ee7f5a05f19d6417a
MuYuee12 <1457403072@qq.com>
2020-08-19 11:46:48 +0800

    cong

--- src/pages/chat/service.js
+++ src/pages/chat/service.js
@@ -2,24 +2,21 @@ import axios from 'axios';
 import { message } from 'antd';
 import api from '../../../util/api';
 
-
-export const getUserByToken = (token) => {
-
-    return axios.get(api.getUserByToken, {
-        params: {
-            token: token,
-        },
-    })
-        .then(res => {
-                if (res.data.status === 200) {
-                    return res.data.data;
-                } else {
-                    message.error(res.data.msg);
-                }
+export const getUserByToken = token => {
+    return axios
+        .get(api.getUserByToken, {
+            params: {
+                token: token,
             },
-        )
+        })
+        .then(res => {
+            if (res.data.status === 200) {
+                return res.data.data;
+            } else {
+                message.error(res.data.msg);
+            }
+        })
         .catch(error => {
-            message.error(error);
+            console.log(error);
         });
-
 };
--- util/api.js
+++ util/api.js
@@ -1,22 +1,18 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const servers = {
     local: 'http://localhost:8001',
-    aliyun1:'http://zmz121.cn:8080',
-    aliyun2:'http://1.zmz121.cn:8080',
-}
+    aliyun1: 'http://zmz121.cn:8080',
+    aliyun2: 'http://1.zmz121.cn:8080',
+    local1: 'http://192.168.2.206',
+};
 
 let server = servers.local;
 
-
 axios.defaults.baseURL = server;
 
-
-
-
 const api = {
-    'getUserByToken': '/user/detail/token'
-}
-
+    getUserByToken: '/user/detail/token',
+};
 
 export default api;
