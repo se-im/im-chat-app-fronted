a6b9cc702ecc05c69e3ca3b5849ff41f629afb4f
MuYuee12 <1457403072@qq.com>
2020-08-20 09:35:05 +0800

    '添加搜索框'

--- src/pages/chat/CvsPanel/index.js
+++ src/pages/chat/CvsPanel/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import styles from './index.css';
 import { Input } from 'antd';
+import { SearchOutlined, PlusCircleOutlined } from '@ant-design/icons';
 
 const index = () => {
     return (
         <div className={styles.cvs_Panel}>
             <div className={styles.cvs_header}>
-                <div>
-                    <span className="iconfont icon-search"></span>
-                </div>
                 <Input
                     size="large"
                     className={styles.search}
-                    placeholder="Search Conversation"
+                    placeholder="Search Conversations"
+                    prefix={<SearchOutlined />}
                 />
+                <div className={styles.add}>
+                    <a className={styles.add_icon}>+</a>
+                </div>
             </div>
+            <div className={styles.cvs_body}></div>
         </div>
     );
 };
