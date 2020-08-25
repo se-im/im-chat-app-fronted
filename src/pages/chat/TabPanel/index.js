93f183a878bf07cfb5b647d86fc65e5df11383d8
MuYuee12 <69440492+MuYuee12@users.noreply.github.com>
2020-08-25 14:12:50 +0800

    '完善TabPanel'

--- src/pages/chat/TabPanel/index.js
+++ src/pages/chat/TabPanel/index.js
@@ -1,23 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './index.css';
 import { Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import { connect } from 'umi';
+import './assert/iconfont.css';
 
-const index = ({user, dispatch}) => {
+class index extends Component {
+    state = {
+        collapsed: false,
+    };
 
-    return (
-        <div className={styles.tab_panel}>
-            <Avatar size={56} icon={<UserOutlined/>} className={styles.be_middle + ' ' + styles.head_pic}/>
-            <h5 className={styles.username}>{user.username}</h5>
-        </div>
-    );
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+    render() {
+        return (
+            <div className={styles.tab_panel}>
+                <Avatar
+                    size={56}
+                    icon={<UserOutlined />}
+                    src={this.props.user.avatarUrl}
+                    className={styles.be_middle + ' ' + styles.head_pic}
+                />
+                <h5 className={styles.username}>
+                    {this.props.user.username} user
+                </h5>
+                <span
+                    className={'iconfont icon-message-fill' + ' ' + styles.icon}
+                />
+                <span className={'iconfont icon-user' + ' ' + styles.icon} />
+                <span
+                    className={
+                        'iconfont icon-fenleiorguangchangorqitatianchong' +
+                        ' ' +
+                        styles.icon
+                    }
+                />
+                <span
+                    className={
+                        'iconfont icon-shezhi' +
+                        ' ' +
+                        styles.icon +
+                        ' ' +
+                        styles.shezhi
+                    }
+                />
+            </div>
+        );
+    }
+}
+
+const handleMsgClick = e => {
+    const dispatch = this.props;
+    dispatch({
+        type,
+    });
 };
-
-
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
     return { user: state.global.user };
 };
-
 
 export default connect(mapStateToProps)(index);
